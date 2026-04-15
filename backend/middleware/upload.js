87a4fc6cfdfdfb89b56c8fc87b98c5cdd3f6478c
Laksh Jain <lakshjain1705@gmail.com>
2026-04-15 14:11:41 +0530

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -3,6 +3,11 @@ const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { Readable } = require('stream');
 
+cloudinary.config({
+  cloud_name: process.env.KEY_NAME,
+  api_key:    process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 // Store in memory — no disk involved
 const upload = multer({
