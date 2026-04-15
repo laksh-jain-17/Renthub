b5fc7099e7908feab38ce49efc047fd2a4908829
Laksh Jain <lakshjain1705@gmail.com>
2026-04-15 10:52:16 +0530

    Update sendOtpEmail.js

--- backend/utils/sendOtpEmail.js
+++ backend/utils/sendOtpEmail.js
@@ -1,5 +1,6 @@
-const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
+
+const { Resend } = require('resend');
 
 const sendOtpEmail = async (toEmail, otp) => {
   await resend.emails.send({
