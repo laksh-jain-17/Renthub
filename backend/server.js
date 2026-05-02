55d18d4a8d4e987603287ece82bc9bb0f2bfdce4
Laksh Jain <lakshjain1705@gmail.com>
2026-05-02 09:51:03 +0530

    Fix CORS allowed origins formatting

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  'http://localhost:5173',
+  'http://localhost:5173', 
   'http://localhost:3000',
 ].filter(Boolean);
 
